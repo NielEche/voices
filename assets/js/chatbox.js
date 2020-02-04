1a281dd6b4141549a309c06c8bc254c135a910f6
Daniel Eche <danieleche7@gmail.com>
2020-02-04 18:06:23 +0100

    NielEche

--- assets/js/chatbox.js
+++
@@ -1,9 +0,0 @@
- function annotate(){
-        var typed= document.getElementById("yourname").value;
-        document.getElementById("printchatbox").innerHTML= typed;
-        }
-    </script>
-    <script>
-        function outputValue(item) {
-            document.getElementById('printchatbox1').innerHTML = item.value;
-        }
---
+++ assets/js/main.js
@@ -0,0 +1,179 @@
+  /* 3 dots dropdown menu */
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  } 
+
+ /* 3 dot dropdown menu end */
+
+
+
+ /* the like button*/
+
+ var like = document.getElementById('like');
+
+ function Toggle() {
+      if(like.classList.contains("far")) {
+     like.classList.remove("far");
+     like.classList.add("fas");
+ } else{
+     like.classList.remove("fas");
+     like.classList.add("far");
+ }
+ }
+
+ /* like button end */
+
+
+
+ /* pagination */
+ var currentTab = 0; // Current tab is set to be the first tab (0)
+    showTab(currentTab); // Display the current tab
+    
+    function showTab(n) {
+      // This function will display the specified tab of the form ...
+      var x = document.getElementsByClassName("tab");
+      x[n].style.display = "block";
+      // ... and fix the Previous/Next buttons:
+      if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+      } else {
+        document.getElementById("prevBtn").style.display = "inline";
+      }
+      if (n == (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = "NEXT>";
+      } else {
+        document.getElementById("nextBtn").innerHTML = "NEXT>";
+      }
+      // ... and run a function that displays the correct step indicator:
+      fixStepIndicator(n)
+    }
+    
+    
+    function nextPrev(n) {
+      // This function will figure out which tab to display
+      var x = document.getElementsByClassName("tab");
+      // Exit the function if any field in the current tab is invalid:
+      if (n == 1 && !validateForm()) return false;
+      // Hide the current tab:
+      x[currentTab].style.display = "none";
+      // Increase or decrease the current tab by 1:
+      currentTab = currentTab + n;
+      // if you have reached the end of the form... :
+      if (currentTab >= x.length) {
+        //...the form gets submitted:
+        currentTab = 0;
+        
+      }
+      // Otherwise, display the correct tab:
+      showTab(currentTab);
+    }
+    
+    function validateForm() {
+      // This function deals with validation of the form fields
+      var x, y, i, valid = true;
+      x = document.getElementsByClassName("tab");
+      y = x[currentTab].getElementsByTagName("input");
+      // A loop that checks every input field in the current tab:
+      for (i = 0; i < y.length; i++) {
+        // If a field is empty...
+        if (y[i].value == "") {
+          // add an "invalid" class to the field:
+          y[i].className += " invalid";
+          // and set the current valid status to false:
+          valid = false;
+        }
+      }
+      // If the valid status is true, mark the step as finished and valid:
+      if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+      }
+      return valid; // return the valid status
+    }
+    
+    function fixStepIndicator(n) {
+      // This function removes the "active" class of all steps...
+      var i, x = document.getElementsByClassName("step");
+      for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+      }
+      //... and adds the "active" class to the current step:
+      x[n].className += " active";
+    }
+
+    /* pagination end */
+
+
+    /* chat box */
+
+    function annotate(){
+        var typed= document.getElementById("yourname").value;
+        document.getElementById("printchatbox").innerHTML= typed;
+        }
+    
+        function outputValue(item) {
+            document.getElementById('printchatbox1').innerHTML = item.value;
+        }
+    /* chat box end */
+
+
+    /* image folder */
+    var loadFile = function(event) {
+        var output = document.getElementById('output');
+        output.src = URL.createObjectURL(event.target.files[0]);
+      };
+    /* image folder end */
+
+
+    /* alerts */
+
+    window.setTimeout(function() {
+        $(".alert").fadeTo(500, 0).slideUp(500, function(){
+            $(this).remove(); 
+        });
+    }, 3000);
+
+    /* alerts end */
+
+
+
+    /* camera upload */
+    
+     // Grab elements, create settings, etc.
+var video = document.getElementById('video');
+
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+    });
+}
+
+// Elements for taking the snapshot
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var video = document.getElementById('video');
+
+// Trigger photo take
+document.getElementById("snap").addEventListener("click", function() {
+	context.drawImage(video, 0, 0, 311, 233);
+});
+
+/* camera upload end */
+
