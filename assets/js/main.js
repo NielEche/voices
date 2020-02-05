3300cc267bab1455831b29b6a144a5bce3b8890f
Daniel Eche <danieleche7@gmail.com>
2020-02-05 17:20:09 +0100

    NielEche

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,3 @@
-  /* 3 dots dropdown menu */
-  function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  } 
-
- /* 3 dot dropdown menu end */
 
 
 
